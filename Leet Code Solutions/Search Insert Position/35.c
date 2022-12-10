073b828a060251d082fd13689cf7409d2a04fdb6
Manoj Kumar H S <103581128+Manoj-2702@users.noreply.github.com>
2022-12-10 08:03:01 +0530

    Search Insert Position

---
+++ Leet Code Solutions/Search Insert Position/35.c
@@ -0,0 +1,16 @@
+int searchInsert(int* nums, int numsSize, int target){
+    int left=0,right=numsSize-1,middle;
+    while(left <= right){
+        middle = (left+right)/2;
+        if(nums[middle]==target){
+            return middle;
+        }
+        else if(nums[middle]<target){
+            left = middle +1;
+        }
+        else{
+            right=middle-1;
+        }
+    }
+    return left;
+}
