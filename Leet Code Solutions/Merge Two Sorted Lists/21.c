6a431eedc0e5e1d210d3a6a767458e33d0e84d37
Manoj Kumar H S <103581128+Manoj-2702@users.noreply.github.com>
2022-12-10 07:35:12 +0530

    Create 21.c

---
+++ Leet Code Solutions/Merge Two Sorted Lists/21.c
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+    if(!list1 && !list2)
+    {
+        return NULL;
+    }
+    if(!list1)
+    {
+        return list2;
+    }
+    if(!list2)
+    {
+        return list1;
+    }
+    struct ListNode *p,*befp;
+    if(list1->val > list2->val)
+    {
+        p=list2;
+        list2=list2->next;
+    }
+    else
+    {
+        p=list1;
+        list1=list1->next;
+    }
+    befp=p;
+    while(list1 && list2)
+    {
+        if(list1->val > list2->val)
+        {
+            p->next=list2;
+            list2=list2->next;
+        }
+        else
+        {
+            p->next=list1;
+            list1=list1->next;
+        }
+        p->next->next=NULL;
+        p=p->next;
+    }
+    if(list1)
+    {
+        p->next=list1;
+    }
+    else if(list2)
+    {
+        p->next=list2;
+    }
+    return befp;
+}
